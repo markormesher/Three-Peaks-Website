2a66a8be0fe16d860853bc5f40850805acdcfbcb
Mark Ormesher <me@markormesher.co.uk>
2015-02-03 21:29:36 +0000

    Twitter and Facebook links

--- src/footer.php
+++ src/footer.php
@@ -4,7 +4,7 @@
 			<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
 				<ul class="list-inline text-center">
 					<li>
-						<a href="https://twitter.com/">
+						<a href="https://twitter.com/m_s_ormo">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
@@ -12,33 +12,19 @@
 						</a>
 					</li>
 					<li>
-						<a href="https://facebook.com/">
+						<a href="https://www.facebook.com/events/762406600480244/">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
                                 </span>
 						</a>
 					</li>
-					<li>
-						<a href="https://github.com/">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
-                                </span>
-						</a>
-					</li>
-					<li>
-						<a href="https://thiefas.de/feeds/rss">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-rss fa-stack-1x fa-inverse"></i>
-                                </span>
-						</a>
-					</li>
 				</ul>
-				<p class="copyright text-muted">Design ported to AnchorCMS by
+				<p class="copyright text-muted">
+					Design ported to AnchorCMS by
 					<a href="https://thiefas.de">ThiefasDE</a> - Original Design by
-					<a href="http://startbootstrap.com" target="_blank">StartBootstrap</a></p>
+					<a href="http://startbootstrap.com" target="_blank">StartBootstrap</a>
+				</p>
 			</div>
 		</div>
 	</div>
