73a5733f37d3aa515f084dd4840ec96bcea171d2
Mark Ormesher <me@markormesher.co.uk>
2015-02-03 23:16:42 +0000

    Donate page template (to override old file)

---
+++ src/donate.php
@@ -0,0 +1,11 @@
+<?php include('_header.php'); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+				<h1>Donate Now</h1>
+				<p>Donate page.</p>
+			</div>
+		</div>
+	</div>
+	<hr>
+<?php include('_footer.php'); ?>
