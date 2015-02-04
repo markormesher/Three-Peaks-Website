688bfeb19cca4ebf7729a201e7aef2c086516304
Mark Ormesher <me@markormesher.co.uk>
2015-02-04 10:59:38 +0000

    Started donate page

--- src/donate.php
+++ src/donate.php
@@ -3,7 +3,25 @@
 		<div class="row">
 			<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
 				<h1>Donate Now</h1>
-				<p>Donate page.</p>
+
+				<p>First of all, <strong><em>thank you</em></strong>. If you're on this page then you're at least thinking about donating, so thank you for that.</p>
+				
+				<p>Donating to our <a href="<?=url('charities.php');?>">chosen charities</a> is really easy: you can donate online, text your donation, or donate with a cheque or cash.</p>
+
+				<hr/>
+
+				<h2>Donate Online</h2>
+				<p>Text...</p>
+
+				<hr/>
+
+				<h2>Text Your Donation</h2>
+				<p>Text...</p>
+
+				<hr/>
+
+				<h2>Cash or Cheque</h2>
+				<p>Text...</p>
 			</div>
 		</div>
 	</div>
