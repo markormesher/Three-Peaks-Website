3d03fc515e4de8646ee9c2d9b31aedb2f28c564f
Mark Ormesher <me@markormesher.co.uk>
2015-02-09 01:09:52 +0000

    Updated donate page

--- src/donate.php
+++ src/donate.php
@@ -5,23 +5,40 @@
 				<h1>Donate Now</h1>
 
 				<p>First of all, <strong><em>thank you</em></strong>. If you're on this page then you're at least thinking about donating, so thank you for that.</p>
-				
 				<p>Donating to our <a href="<?=url('charities.php');?>">chosen charities</a> is really easy: you can donate online, text your donation, or donate with a cheque or cash.</p>
 
 				<hr/>
 
 				<h2>Donate Online</h2>
-				<p>Text...</p>
+				<p>We have set up a team page with Just Giving where you can donate to any one of <a href="<?=url('charities.php');?>">our charities</a> quickly, easily and very safely.</p>
+				<p><a href="https://www.justgiving.com/teams/threepeaks2015" rel="external">&raquo; Go to the Team Page</a></p>
 
 				<hr/>
 
 				<h2>Text Your Donation</h2>
-				<p>Text...</p>
+				<p>You can donate to any one of <a href="<?=url('charities.php');?>">our charities</a> straight from your phone!</p>
+				<table>
+					<tr>
+						<td>To donate to <em>BHF</em>...</td>
+						<td style="width: 40px;">&nbsp;</td>
+						<td>Text <strong>TPKS<span style="text-decoration: underline">55</span> &pound;3</strong> to 70070</td>
+					</tr>
+					<tr>
+						<td>To donate to <em>JDRF</em>...</td>
+						<td style="width: 40px;">&nbsp;</td>
+						<td>Text <strong>TPKS<span style="text-decoration: underline">56</span> &pound;3</strong> to 70070</td>
+					</tr>
+					<tr>
+						<td>To donate to <em>RTR</em>...</td>
+						<td style="width: 40px;">&nbsp;</td>
+						<td>Text <strong>TPKS<span style="text-decoration: underline">57</span> &pound;3</strong> to 70070</td>
+					</tr>
+				</table>
 
 				<hr/>
 
 				<h2>Cash or Cheque</h2>
-				<p>Text...</p>
+				<p>If you know anyone in the team (Andy, Iggy or Mark) please feel free to donate directly to us and we will make sure the cash reaches the charities we are supporting.</p>
 			</div>
 		</div>
 	</div>
