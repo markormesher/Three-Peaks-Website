566462e986a6031d0950f014ddc9ce3e685636cc
Mark Ormesher <me@markormesher.co.uk>
2015-02-09 01:31:02 +0000

    Added donation link to post

--- src/posts/2015-02-09-training-starts-today.php
+++ src/posts/2015-02-09-training-starts-today.php
@@ -13,6 +13,7 @@
 						<p>Today, I start my training programme for the <strong>Three Peaks Challenge</strong>. We still haven't secured a sponsor (we have a few promising leads!), but we can't let that be a reason to delay training.</p>
 						<p>To climb <em>3 mountains</em> in <em>3 countries</em> in under <em>24 hours</em> we will need to be in peak physical fitness. We need to develop the stamina to climb over 3km and walk for longer than the London marathon, and the mental focus to survive a punishing cycle of physical exertion and sleep deprivation.</p>
 						<p>My new regime ranges from gym sessions focusing on my legs and core to long (<em>long</em>) walks on the weekends. Today is legs and core at the gym, followed by a 4mi bike ride. <strong>Wish me luck!</strong></p>
+						<p><strong>Want to donate?</strong> Awesome! From pennies to pounds, every donation is appreciated. <a href="<?=url('donate');?>">Find out how to donate</a>.</p>
 						<hr />
 						<p><em>Side note:</em> if any of my London friends want to join me on my weekend walks (starting at 3 hours, climbing to 7-8 hours in May) please do let me know - you'd be more than welcome!</p>
 					</div>
