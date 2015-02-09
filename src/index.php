1eb0f2d075623d94227b491eb52e59ba63e6be2a
Mark Ormesher <me@markormesher.co.uk>
2015-02-09 01:26:03 +0000

    1st p0st!

--- src/index.php
+++ src/index.php
@@ -4,11 +4,11 @@
 		<div class="row">
 			<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
 				<div class="post-preview">
-					<a href="<?=url('post.php');?>" title="">
-						<h2 class="post-title">Post Title</h2>
-						<h3 class="post-subtitle">Sub Title</h3>
+					<a href="<?=url('posts/2015-02-09-training-starts-today.php');?>" title="Training starts today">
+						<h2 class="post-title">Training Starts Today</h2>
+						<h3 class="post-subtitle">And it's going to be tough...</h3>
 					</a>
-					<p class="post-meta">Written by Mark Ormesher on 3rd February 2015 @ 20:58</p>
+					<p class="post-meta">Written by Mark Ormesher on 9th February 2015 @ 07:12</p>
 				</div>
 				<hr>
 			</div>
---
+++ src/posts/2015-02-09-training-starts-today.php
@@ -0,0 +1,25 @@
+<?php include('../_header.php'); ?>
+
+	<section class="content wrap">
+		<article>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+						<h1>Training Starts Today</h1>
+						<h5>Written by Mark Ormesher | 9th February 2015 @ 07:12</h5>
+						<hr/>
+						<p>It's 7am on a Monday morning, my lecture doesn't start for another 8 hours, and I have no coursework outstanding. Normally, this means I'd be asleep right now for at least a few more hours.</p>
+						<p><strong>But not this week.</strong> And not for the next 16 weeks either.</p>
+						<p>Today, I start my training programme for the <strong>Three Peaks Challenge</strong>. We still haven't secured a sponsor (we have a few promising leads!), but we can't let that be a reason to delay training.</p>
+						<p>To climb <em>3 mountains</em> in <em>3 countries</em> in under <em>24 hours</em> we will need to be in peak physical fitness. We need to develop the stamina to climb over 3km and walk for longer than the London marathon, and the mental focus to survive a punishing cycle of physical exertion and sleep deprivation.</p>
+						<p>My new regime ranges from gym sessions focusing on my legs and core to long (<em>long</em>) walks on the weekends. Today is legs and core at the gym, followed by a 4mi bike ride. <strong>Wish me luck!</strong></p>
+						<hr />
+						<p><em>Side note:</em> if any of my London friends want to join me on my weekend walks (starting at 3 hours, climbing to 7-8 hours in May) please do let me know - you'd be more than welcome!</p>
+					</div>
+				</div>
+			</div>
+		</article>
+		<hr/>
+	</section>
+
+<?php include('../_footer.php'); ?>
