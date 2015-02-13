78c34f792e15325e6a703a8d0ef407fe8b64ebe4
markormesher <me@markormesher.co.uk>
2015-02-13 20:21:06 +0000

    Added charity bio post

---
+++ src/posts/2015-02-13-who-are-we-helping.php
@@ -0,0 +1,58 @@
+<?php include('../_header.php'); ?>
+
+	<section class="content wrap">
+		<article>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+						<h1>Who Are We Helping?</h1>
+						<h5>Written by Mark Ormesher | 13th February 2015 @ 16:41</h5>
+						<hr/>
+
+						<p>Our challenge involves <strong>three mountains</strong> and
+							<strong>three climbers</strong>, so it made sense to select <strong>three charities</strong> to support. We have decided, for a range of reasons, to support the following
+							<strong><a href="<?= url('charities'); ?>">three awesome causes</a></strong>:</p>
+						<ul>
+							<li>British Heart Foundation</li>
+							<li>Juvenile Diabetes Research Foundation</li>
+							<li>Room to Read</li>
+						</ul>
+						<p>To give you an idea of why we chose these causes and what they're doing, we've given a little bio on each of the charities below:</p>
+
+						<hr/>
+
+						<h2>British Heart Foundation (BHF)</h2>
+						<h4>Text &quot;<em>TPKS55 &pound;5</em>&quot; to 70070</h4>
+						<p>Coronary heart disease is the single biggest killer in the UK. BHF are leading the fight against cardiovascular disease and driving towards a world in which it cannot cause any suffering or premature death.</p>
+						<p>The BHF funds research into a range of areas, intended to bring about a better understand of the causes of heart disease and explore avenues to prevent and cure it.</p>
+
+						<hr/>
+
+						<h2>Juvenile Diabetes Research Foundation (JDRF)</h2>
+						<h4>Text &quot;<em>TPKS56 &pound;5</em>&quot; to 70070</h4>
+						<p>Diabetes cannot be cured, and without proper treatment it can very quickly put a person's life at risk. JDRF fund important research to cure, treat and prevent Type 1 diabetes (also known as juvenile diabetes).</p>
+						<p>Because diabetes is an (as yet) uncurable condition that can seriously impact quality of life, JDRF also supports people living with the condition by giving them a collective voice to campaign for greater research funding and access to treatments.</p>
+
+						<hr/>
+
+						<h2>Room to Read (RTR)</h2>
+						<h4>Text &quot;<em>TPKS57 &pound;5</em>&quot; to 70070</h4>
+						<p>Room to Read are working to ensure that all children, throughout the world, can pursue a quality education and reach their full potential. To do this, they focus on two main areas: improving literacy, and gender equality in education.</p>
+						<p>They work primarily across Asia and Africa to develop literacy stills and promote equality by providing books, libraries, support, education and more. In 2012, nearly 10 million books were checked out from RTR libraries.</p>
+
+						<hr/>
+
+						<p>Want to find out more about <a href="<?= url('charities'); ?>">our amazing causes</a>? They all have websites, which you can find below:</p>
+						<ul>
+							<li><a href="http://www.bhf.org.uk/" rel="external">British Heart Foundation</a></li>
+							<li><a href="http://www.jdrf.org.uk." rel="external">Juvenile Diabetes Research Foundation</a></li>
+							<li><a href="http://http://www.roomtoread.org/" rel="external">Room to Read</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</article>
+		<hr/>
+	</section>
+
+<?php include('../_footer.php'); ?>
